36ad2f2fc5cf1f2d673a806f4695a8e8958eeee4
ftardone <33504644+ftardone@users.noreply.github.com>
2022-04-29 19:44:38 -0400

    v1

---
+++ Mutant/Controllers/mutantController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mutant.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class mutantController : Controller
+    {
+        [HttpPost]
+        public IActionResult Index(string[] dna)
+        {
+            //return View();
+            bool response = false;
+            int count_A = 0;
+            int count_T = 0;
+            int count_C = 0;
+            int count_G = 0;
+            int coincidencias = 0;
+
+            foreach (string item in dna)
+            {
+                if (item.ToLower().Count(f => f == 'a') > 3) { coincidencias++; }
+                if (item.ToLower().Count(f => f == 't') > 3) { coincidencias++; }
+                if (item.ToLower().Count(f => f == 'c') > 3) { coincidencias++; }
+                if (item.ToLower().Count(f => f == 'g') > 3) { coincidencias++; }
+
+                if (coincidencias > 1) { response = true; break; }
+            }
+
+            return response ? Ok() : StatusCode(403);
+        }
+    }
+}
